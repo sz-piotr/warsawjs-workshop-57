7a4040235552d16a5f93607fa3952d62adc479e6
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-15 11:56:36 +0200

    Add note about key

--- src/tasks/06-if-for-lesson/IfForLesson.tsx
+++ src/tasks/06-if-for-lesson/IfForLesson.tsx
@@ -4,6 +4,7 @@ We'll cover the following topics:
 - conditional rendering using &&, and how React renders false and null
 - conditional rendering using ? :
 - list rendering using .map
+- remembering about using key
 - repeating something n times using new Array(n).fill(0)
 
 */
