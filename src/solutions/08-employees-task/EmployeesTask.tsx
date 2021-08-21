a90b2a825c4e90b2bd92040d1d9e5a768f39dfaa
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-21 17:50:58 +0200

    Add 08 solution

---
+++ src/solutions/08-employees-task/EmployeesTask.tsx
@@ -0,0 +1,63 @@
+/*
+
+Task:
+Render a list of employees that displays their details. If a person is a manager
+they should have [Manager] before their name.
+
+Example:
+
+* [Manager] Lisa Green
+  Salary: $60000
+* Bob Murphy
+  Salary: $25000
+
+*/
+
+const employees = [
+  {
+    firstName: "Lisa",
+    lastName: "Green",
+    isManager: true,
+    salary: 60_000,
+  },
+  {
+    firstName: "Bob",
+    lastName: "Murphy",
+    isManager: false,
+    salary: 25_000,
+  },
+  {
+    firstName: "Carl",
+    lastName: "Anderson",
+    isManager: false,
+    salary: 30_000,
+  },
+  {
+    firstName: "Ronald",
+    lastName: "Smith",
+    isManager: true,
+    salary: 57_000,
+  },
+  {
+    firstName: "Amy",
+    lastName: "Miller",
+    isManager: false,
+    salary: 29_000,
+  },
+];
+
+export function EmployeesTask() {
+  return (
+    <ul>
+      {employees.map((employee, i) => (
+        <li key={i}>
+          <div>
+            {employee.isManager && "[Manager] "}
+            {employee.firstName} {employee.lastName}
+          </div>
+          <div>Salary: ${employee.salary}</div>
+        </li>
+      ))}
+    </ul>
+  );
+}
